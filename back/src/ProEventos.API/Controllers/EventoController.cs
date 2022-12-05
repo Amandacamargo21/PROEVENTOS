7942b29356ce15d95ff9b3a3873f95e2479c12e5
AmandaCamargo21 <amandacamargo2102@gmail.com>
2022-12-05 17:27:14 -0300

    config data context e criação do banco

---
+++ back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return new Evento[]
+            {
+                new Evento()
+                {
+                    EventoId = 1,
+                    Tema = "Angular 11 e .NET 5",
+                    Local = "BH",
+                    Lote = "1º Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString()
+                }
+        };
+        }
+
+    [HttpPost]
+    public string Post()
+    {
+        return "Exemplo de Post";
+    }
+}
+}
